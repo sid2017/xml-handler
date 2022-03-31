4240106d55e3ebb84c28436e6823aaac5b0021c4
sid2017 <m.vondufais@gmx.de>
2022-03-31 15:40:24 +0200

    added parser_3.py

---
+++ parser_3.py
@@ -0,0 +1,42 @@
+import argparse
+import glob
+import xml.etree.ElementTree as ET
+
+doi_dump = []
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Export DOIs from xml files')
+
+    parser.add_argument(
+        "-f", "--files",
+        type=str,
+        nargs="+",
+        help='enter target files')
+
+    args = parser.parse_args()
+
+    file_list = args.files
+
+    """filenames = glob.glob("*.xml")  # change the pattern to match your case"""
+
+    for filename in file_list:
+
+        with open(filename, 'r', encoding="utf-8") as content:
+
+            tree = ET.parse(content)
+
+            lst_dois = tree.findall('.//article-id')
+
+            for doi in lst_dois:
+
+                doi_dump.append(doi.text)
+
+with open('chapter_dois.txt', 'w') as f:
+    for i in doi_dump:
+        f.write(i)
+        f.write('\n')
+
+print("DOIs extracted: \n ------------------------------")
+for item in doi_dump:
+    print(item)
