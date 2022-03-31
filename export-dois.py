b34ce53df324ce392833ed1a83533bb3ef6a7a47
sid2017 <m.vondufais@gmx.de>
2022-03-31 17:57:03 +0200

    cleanup

--- export-dois.py
+++ export-dois.py
@@ -22,10 +22,6 @@ if __name__ == "__main__":
         type=str,
         nargs="+",
         help='enter target files')
-    parser.add_argument(
-        "-a", "--all",
-        help='target all .xml files in current directory'
-    )
 
     args = parser.parse_args()
 
