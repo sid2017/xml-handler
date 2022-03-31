88afe07ab731dbf30a80976d61530bb184510c1b
sid2017 <m.vondufais@gmx.de>
2022-03-31 21:28:27 +0200

    Added some comments for clarification

--- export-dois.py
+++ export-dois.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 
 doi_dump = []
 
+# Function 1: Search for DOI in target files
 def search_doi(file_list):
  for filename in file_list:
         with open(filename, 'r', encoding="utf-8") as content:
@@ -14,6 +15,7 @@ def search_doi(file_list):
 
 if __name__ == "__main__":
 
+    # Prase CLI argmuments
     parser = argparse.ArgumentParser(description='Export DOIs from xml files')
 
     parser.add_argument(
@@ -31,11 +33,13 @@ if __name__ == "__main__":
         file_list = glob.glob("*.xml")
         search_doi(file_list)
 
+# Create output file
 with open('chapter_dois.txt', 'w') as f:
     for i in doi_dump:
         f.write(i)
         f.write('\n')
 
+# Terminal output
 print("-------> DOIs extracted:")
 for item in doi_dump:
     print(item)
