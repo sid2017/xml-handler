2b2c7f7e027afe751fea6334715a4f2ab24bb2ce
sid2017 <m.vondufais@gmx.de>
2022-03-31 17:41:41 +0200

    modified parser_3.py

--- parser_3.py
+++ parser_3.py
@@ -4,6 +4,15 @@ import xml.etree.ElementTree as ET
 
 doi_dump = []
 
+def main(file_list):
+ for filename in file_list:
+        with open(filename, 'r', encoding="utf-8") as content:
+            tree = ET.parse(content)
+            lst_dois = tree.findall('.//article-id')
+            for doi in lst_dois:
+                doi_dump.append(doi.text)
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Export DOIs from xml files')
@@ -22,25 +31,16 @@ if __name__ == "__main__":
 
     file_list = args.files
 
+    main(file_list)
+
     """filenames = glob.glob("*.xml")  # change the pattern to match your case"""
 
-    for filename in file_list:
-
-        with open(filename, 'r', encoding="utf-8") as content:
-
-            tree = ET.parse(content)
-
-            lst_dois = tree.findall('.//article-id')
-
-            for doi in lst_dois:
-
-                doi_dump.append(doi.text)
 
 with open('chapter_dois.txt', 'w') as f:
     for i in doi_dump:
         f.write(i)
         f.write('\n')
 
-print("DOIs extracted: \n ------------------------------")
+print("-------> \n-------> DOIs extracted: \n------->")
 for item in doi_dump:
     print(item)
